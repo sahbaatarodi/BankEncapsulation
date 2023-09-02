c16eaddf3c97fbb9158ff006fe61df88b6a24d4f
sahba atarodi <sahba.atarodi@gmail.com>
2023-09-02 15:42:54 -0400

    initial commit

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much money do you want to deposit ?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+
+            double userBalance = ba.GetBalance();
+            Console.WriteLine($"Your current balance is: {userBalance, 0:c}");
         }
     }
 }
